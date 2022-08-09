ee508da0f1f7ff56fe127ac7e5b19497817b708c
charlie-millie <charlie@millie.town>
2022-08-09 19:08:09 +0900

    - work

--- src/script/entities/Entity.ts
+++ src/script/entities/Entity.ts
@@ -1,5 +1,8 @@
+import {Vec2} from "../Math";
+
 export class Entity {
     //audio
+    public pos = new Vec2();
 
 
 }
--- src/script/entities/index.ts
+++ src/script/entities/index.ts
@@ -1,4 +1,5 @@
 
+
 export async function loadEntities(audioContext: AudioContext): Promise<T> {
 
 }
